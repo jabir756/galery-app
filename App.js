652609c5e3b8acf8fa055658155e62047d2a4dcc
jabir756 <jabirkagone6@gmail>
2024-11-26 14:40:19 -0500

    Add photo gallery feature with image grid layout

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Image } from 'react-native';
+import images from './images';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="auto"/>
+
+      <ScrollView style={styles.scroller}>
+        <Text style={styles.title}>Galerie de Photos</Text>
+        <View style={styles.grid}>
+          {images.map((image, index) => (
+            <View key={index} style={styles.gridItem}>
+              <Image source={image} style={styles.image} />
+            </View>
+          ))}
+        </View>
+      </ScrollView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  scroller: {
     flex: 1,
+    marginTop: 20,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginVertical: 20,
+  },
+  grid: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  gridItem: {
+    width: '45%', // Ajustez la largeur selon vos besoins
+    marginVertical: 10,
+  },
+  image: {
+    width: '100%',
+    height: 150,
+    borderRadius: 10,
   },
 });
---
+++ images.js
@@ -0,0 +1,16 @@
+const images = [
+  require('./img/anthony-nelzin-santos-fF1DWEA1qB8-unsplash.jpg'),
+  require('./img/bruno-ngarukiye-uF2B40niSFs-unsplash.jpg'),
+  require('./img/fabrizio-coco-NGktqjG777E-unsplash.jpg'),
+  require('./img/frank-ching-6IbhierWi0Y-unsplash.jpg'),
+  require('./img/janis-dzenis-oMds2-7ytrg-unsplash.jpg'),
+  require('./img/keith-tanner-DEozOZvPvxw-unsplash.jpg'),
+  require('./img/keith-tanner-VBhRyeyY6qc-unsplash.jpg'),
+  require('./img/miraxh-tereziu-Vq0qCv0Wc-o-unsplash.jpg'),
+  require('./img/samsung-memory-l_p4pay8CTM-unsplash.jpg'),
+  require('./img/samsung-memory-nplkFSNschY-unsplash.jpg'),
+  require('./img/tommao-wang-CMjSM6MmUtg-unsplash.jpg'),
+  require('./img/yue-wu-NwmHxVS7W8k-unsplash.jpg'),
+];
+
+export default images;
